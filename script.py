ee1a0f0f7981594afc41a26786479b6369d9fb5b
Eric van Rees <ericvanrees@gmail.com>
2025-06-12 08:01:51 +0200

    third commit

--- script.py
+++ script.py
@@ -78,7 +78,7 @@ a, b, *_ = (1, 2, 3, 4, 5)
 # or:
 a, b, *c, d = (1, 2, 3, 4, 5)
 
-# 7. Getting and setting attributes on a certain object
+# 7. Getting and setting attributes on a certain object using setattr
 
 
 class Person():
@@ -90,6 +90,60 @@ person = Person()
 first_key = 'first'
 first_val = 'Corey'
 
-setattr(person, 'first', 'Corey')
+# setattr allows you to pass in variables: 
+#setattr(person, first_key, first_val)
+# this is the same as this:
+# setattr(person, 'first', 'Corey')
 
-print(person.first)
+# getattr works similarly:
+#first = getattr(person, first_key)
+
+#print(person.first) # Corey
+
+# example with a loop:
+person_info = {'first': 'Corey', 'last': 'Schafer'}
+
+for key, value in person_info.items():
+    setattr(person, key, value)
+    
+print(person.first) # Corey
+print(person.last) # Schafer
+
+# this gives the same result:
+for key in person_info.keys():
+    print(getattr(person, key))
+    
+
+# 8. inputting secret information
+
+# the wrong way to do this is seeting it displayed when typing it
+username = input('Username: ')
+password = input('Password: ')
+
+print('Logging In...')
+
+# Python has a built-in function for this called getpass from the standard library
+
+from getpass import getpass
+
+username = input('Username: ')
+password = getpass('Password: ')
+
+# 9. Running Python with -m option
+
+# python -m venv my_env
+# python -m smtpd -c DebuggingServer -n localhost:1025
+# -m stands for module (searches for it outside of cwd, inside sys.path so on your system. You need to add -m to run a module on your system, what follows are arguments on how to run it).
+
+# 10. How to learn more about certain objects with helpful builtin functions
+
+# import smtpd
+# use help function:
+# help(smtpd)
+# dir(object) gives you all available methods and attributes:
+# from datetime import datetime
+# dir(datetime)
+# datetime.today 
+# tells you it's a method, now run it to see what it does:
+# datetime.today()
+
